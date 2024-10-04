3694aabeadb05a54dfbc8705f784832c366981f4
socuser1 <socuser1@domain.local>
2024-10-04 18:06:22 +0800

    Added marquee in h1

---
+++ script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h2 style="text-align: center; color: green;">
+        Welcome To GFG
+    </h2>
+
+    <marquee> Lets Move this text.</marquee>
+    
+        style="border:black 2px solid">
+        Geeks for Geeks
+    </marquee>
+</body>
+
+</html>
